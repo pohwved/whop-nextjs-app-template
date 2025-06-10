500c75b48de4dc813783117e69321d0bba19346b
pohwved <refsterpohw@gmail.com>
2025-06-10 07:30:40 +0000

    asdfasdfsadf2332

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		remotePatterns: [{ hostname: "**" }],
-	},
-};
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "*.app.github.dev"],
+    },
+  },
+}
 
-export default nextConfig;
+export default nextConfig
