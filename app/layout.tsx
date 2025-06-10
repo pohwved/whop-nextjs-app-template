10a36b822ade285877bf93c7becc42589b45ceb4
pohwved <refsterpohw@gmail.com>
2025-06-10 07:07:30 +0000

    jklghjjfghj

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<Theme accentColor="blue">
 					{children}
-				</Theme>
 			</body>
 		</html>
 	);
